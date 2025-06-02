a9b5d8bcf4c8eeedeb4a608d677f6bbb32462cc9
NathanFallet <contact@nathanfallet.me>
2025-06-02 14:43:39 +0200

    adding test evaluate with promise

--- core/src/jvmTest/kotlin/dev/kdriver/core/tab/TabTest.kt
+++ core/src/jvmTest/kotlin/dev/kdriver/core/tab/TabTest.kt
@@ -3,8 +3,10 @@ package dev.kdriver.core.tab
 import dev.kdriver.core.browser.Browser
 import dev.kdriver.core.sampleFile
 import kotlinx.coroutines.runBlocking
+import kotlinx.serialization.json.jsonPrimitive
 import kotlin.test.Test
 import kotlin.test.assertEquals
+import kotlin.test.assertFailsWith
 import kotlin.test.assertNotNull
 
 class TabTest {
@@ -46,6 +48,31 @@ class TabTest {
 
         assertEquals(existingUserAgent, navigatorUserAgent)
         assertEquals("testLang", navigatorLanguage)
+        browser.stop()
+    }
+
+    @Test
+    fun testEvaluateWaitPromiseSuccess() = runBlocking {
+        val browser = Browser.create(this, headless = true, sandbox = false)
+        val tab = browser.mainTab ?: throw IllegalStateException("Main tab is not available")
+
+        val result = tab.evaluate<String>("new Promise(r => setTimeout(() => r(\"ok\")));", true)
+
+        assertNotNull(result)
+        assertEquals("ok", result)
+        browser.stop()
+    }
+
+    @Test
+    fun testEvaluateWaitPromiseError() = runBlocking {
+        val browser = Browser.create(this, headless = true, sandbox = false)
+        val tab = browser.mainTab ?: throw IllegalStateException("Main tab is not available")
+
+        val result = assertFailsWith<EvaluateException> {
+            tab.evaluate<String>("new Promise((_, r) => setTimeout(() => r(\"fail\")));", true)
+        }
+
+        assertEquals("fail", result.error.exception?.value?.jsonPrimitive?.content)
         browser.stop()
     }
 
