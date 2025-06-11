2f9c3b4bfb4360cee79d8a4fe897115a7c0b1482
NathanFallet <contact@nathanfallet.me>
2025-06-11 22:18:33 +0200

    element get attributes + docs

--- core/src/commonMain/kotlin/dev/kdriver/core/dom/Element.kt
+++ core/src/commonMain/kotlin/dev/kdriver/core/dom/Element.kt
@@ -17,9 +17,18 @@ data class Element(
 
     private var remoteObject: Runtime.RemoteObject? = null
 
+    /**
+     * The name of the tag of the element, in lowercase.
+     *
+     * For example, if the element is a `<div>`, this will return "div".
+     */
     val tag: String
         get() = node.nodeName.lowercase()
 
+    /**
+     * The text content of the element, which is the concatenation of all text nodes
+     * within the element, excluding any HTML tags.
+     */
     val text: String
         get() = filterRecurse(
             node,
@@ -28,6 +37,10 @@ data class Element(
             getShadowRoots = { it.shadowRoots }
         )?.nodeValue ?: ""
 
+
+    /**
+     * The internal node ID of the element in the DOM tree.
+     */
     val backendNodeId: Int
         get() = node.backendNodeId
 
@@ -59,6 +72,30 @@ data class Element(
         // displays for a short time a red dot on the element (only if the element itself is visible)
     }
 
+    /**
+     * Returns the value of the attribute with the given name, or null if it does not exist.
+     *
+     * For example, if the element has an attribute `class="my-class"`, you can retrieve it with:
+     * ```kotlin
+     * val classValue = element["class"]
+     * ```
+     *
+     * @param name The name of the attribute to retrieve.
+     * @return The value of the attribute, or null if it does not exist.
+     */
+    operator fun get(name: String): String? {
+        val keyIndex = node.attributes?.indexOfFirst { it == name }?.takeIf { it != -1 } ?: return null
+        return node.attributes?.get(keyIndex + 1)
+    }
+
+    /**
+     * Clicks the element, simulating a user click.
+     *
+     * This method resolves the element's remote object and calls the `click` function on it.
+     * It also flashes the element for a short duration to indicate the click action.
+     *
+     * @throws IllegalStateException if the remote object cannot be resolved.
+     */
     suspend fun click() {
         remoteObject = tab.dom.resolveNode(backendNodeId = backendNodeId).`object`
         val objectId = remoteObject?.objectId ?: throw IllegalStateException("Could not resolve object id for $this")
@@ -76,6 +113,12 @@ data class Element(
         )
     }
 
+    /**
+     * Moves the mouse to the center of the element and simulates a mouse click.
+     *
+     * This method retrieves the position of the element, moves the mouse to that position,
+     * and dispatches mouse events to simulate a click.
+     */
     suspend fun mouseMove() {
         val position = getPosition()
         if (position == null) {
@@ -98,6 +141,16 @@ data class Element(
         )
     }
 
+    /**
+     * Sends a sequence of keys to the element, simulating user input.
+     *
+     * This method focuses the element and dispatches key events for each character in the provided text.
+     *
+     * If the text contains special characters or needs to be handled differently,
+     * prefer calling `sendKeysWithSpecialChars(text: String)` instead.
+     *
+     * @param text The text to send to the element.
+     */
     suspend fun sendKeys(text: String) {
         apply("(elem) => elem.focus()")
         for (char in text) tab.input.dispatchKeyEvent(
@@ -106,6 +159,13 @@ data class Element(
         )
     }
 
+    /**
+     * Sends a list of file paths to the element, simulating a file input.
+     *
+     * This method sets the files for the element's file input using the provided paths.
+     *
+     * @param paths A list of file paths to send to the element.
+     */
     suspend fun sendFiles(paths: List<Path>) {
         tab.dom.setFileInputFiles(
             files = paths.map { it.toString() },
@@ -114,6 +174,18 @@ data class Element(
         )
     }
 
+    /**
+     * Applies a JavaScript function to the element and returns the result. The given js_function string should accept the js element as parameter,
+     * and can be a arrow function, or function declaration.
+     *
+     * Examples of valid JavaScript functions:
+     * - `(elem) => { elem.value = "blabla"; console.log(elem); alert(JSON.stringify(elem)); }`
+     * - `elem => elem.play()`
+     * - `function myFunction(elem) { alert(elem) }`
+     *
+     * @param jsFunction The JavaScript function to apply to the element.
+     * @return The result of the function call, or null if the result is not serializable.
+     */
     suspend fun apply(jsFunction: String): JsonElement? {
         remoteObject = tab.dom.resolveNode(backendNodeId = backendNodeId).`object`
 
@@ -130,6 +202,15 @@ data class Element(
         return result.result.value
     }
 
+    /**
+     * Retrieves the position of the element in the viewport.
+     *
+     * This method calculates the position of the element based on its content quads.
+     * If `abs` is true, it returns the absolute position relative to the document.
+     *
+     * @param abs If true, returns the absolute position; otherwise, returns the relative position.
+     * @return The position of the element, or null if it could not be determined.
+     */
     suspend fun getPosition(abs: Boolean = false): Position? {
         if (remoteObject == null || parent == null || objectId == null) {
             remoteObject = tab.dom.resolveNode(backendNodeId = backendNodeId).`object`
