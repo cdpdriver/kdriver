5d136782ff780bbce284b69fce99440562d52f3b
NathanFallet <contact@nathanfallet.me>
2025-10-31 17:33:03 +0100

    fix mingw missing exists implementation

--- cdp-generate/build.gradle.kts
+++ cdp-generate/build.gradle.kts
@@ -9,7 +9,7 @@ repositories {
 
 kotlin {
     dependencies {
-        implementation("org.jetbrains.kotlin:kotlin-gradle-plugin:2.1.10")
+        implementation("org.jetbrains.kotlin:kotlin-gradle-plugin:2.1.21")
         implementation("com.squareup:kotlinpoet:2.2.0")
     }
 }
